676addccb804a8d269fd20b2312e0613ddb17de2
fatou fall <fatoufall95@gmail.com>
2024-12-09 02:38:49 -0500

    able render articles by id

--- server/routes/main.js
+++ server/routes/main.js
@@ -4,15 +4,55 @@ const Post = require("../models/Post");
 
 //routes , GET HOME
 router.get("", async (req, res) => {
-  const locals = {
-    title: "Node Blog",
-    description: "simple blog created with nodeJs, express & mongoDB",
-  };
   try {
-    const data = await Post.find();
-    res.render("index", { locals, data });
+    const locals = {
+      title: "Node Blog",
+      description: "Simple blog created with Node.js, Express, & MongoDB",
+    };
+    let perPage = 10;
+    let page = req.query.page || 1;
+
+    const data = await Post.aggregate([{ $sort: { createdAt: -1 } }])
+      .skip(perPage * page - perPage)
+      .limit(perPage)
+      .exec();
+
+    const count = await Post.countDocuments();
+    const nextPage = parseInt(page) + 1;
+    const hasNextPage = nextPage <= Math.ceil(count / perPage);
+
+    res.render("index", {
+      locals,
+      data,
+      current: page,
+      nextPage: hasNextPage ? nextPage : null,
+    });
   } catch (error) {
-    console.log(404);
+    console.error("Error occurred:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+// GET post :ID
+
+router.get("/post/:id", async (req, res) => {
+  try {
+    let slug = req.params.id;
+
+    const data = await Post.findById({ _id: slug });
+
+    const locals = {
+      title: data.title,
+      description: "Simple Blog created with NodeJs, Express & MongoDb.",
+    };
+
+    res.render("post", {
+      locals,
+      data,
+      currentRoute: `/post/${slug}`,
+    });
+  } catch (error) {
+    console.log(error);
   }
 });
 
