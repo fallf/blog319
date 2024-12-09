0f407a45fbeda1a5b4293e5fc5d38588830bac47
fatou fall <fatoufall95@gmail.com>
2024-12-08 23:01:24 -0500

    added main page and pic

--- routes/main.js
+++ routes/main.js
@@ -4,11 +4,15 @@ const router = express.Router();
 //routes
 router.get("", (req, res) => {
   const locals = {
-    title: "node Blog",
+    title: "Node Blog",
     description: "simple blog created with nodeJs, express & mongoDB",
   };
 
   res.render("index", { locals });
+});
+
+router.get("/about", (req, res) => {
+  res.render("about");
 });
 
 module.exports = router;
